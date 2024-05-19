cc3a86f21a15d69f5da6cb3c53e4db11d0327b83
Mehmt Yigit Metin <mehmetyigitmetinn@gmail.com>
2024-05-19 18:42:23 +0300

    js2

--- jsbasics.js
+++ jsbasics.js
@@ -2,11 +2,13 @@
 ? How many types of data do JS have?
 
 ! DATA TYPES:
+--------- PRIMITIVE DATA TYPE-----------
 String
 Number
-Boolean
+Boolean (true and false)
 Null
 Undefined
+---------- REFERENCE DATA TYPE ------------
 Object
 Function
 
@@ -19,7 +21,47 @@ let numberStringg = "87"; // Mentioned that if you want string number like "12" 
 
 console.log(number + numberStringg); // This can give an output as 8787.
 
+// !BOOLEAN
+/*
+let a = 23;
+let b = 12;
 
+let sonuc = a+b;
+console.log(sonuc = 35);
+*/
 
+// ? NULLLLLL ( boş demektir. Bir variable'ın içini boşaltmak için kullanılır. )
 
+/*
+let a = null;
 
+a=23;
+
+console.log(a);
+*/
+
+// TODO ///// UNDEFINED (Tanımsız veri tipidir.)
+
+/*
+let a;
+console.log(a);
+*/
+
+// ------------ OBJECT ----------
+let human = {
+  name: "Yigit",
+  surname: "Metin",
+  age: 24,
+};
+
+console.log(human);
+
+// ----------- FUNCTIONS ---------
+
+function rewrite() {
+  if (24 > 23) {
+    console.log("You cannot passed :(");    
+  }
+}
+
+console.log(rewrite);
