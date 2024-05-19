f932b004215c01f41f1339b89b0956d8715e2007
Mehmt Yigit Metin <mehmetyigitmetinn@gmail.com>
2024-05-19 18:13:00 +0300

    js

---
+++ jsbasics.js
@@ -0,0 +1,25 @@
+/* 
+? How many types of data do JS have?
+
+! DATA TYPES:
+String
+Number
+Boolean
+Null
+Undefined
+Object
+Function
+
+*/
+
+let name1 = "Yigit"; // ----------> String
+
+let number = 87;
+let numberStringg = "87"; // Mentioned that if you want string number like "12" instead of the real number like 12, you must use double quote.
+
+console.log(number + numberStringg); // This can give an output as 8787.
+
+
+
+
+
